769a9df493e1eb1e88f22c7581c78366359af63c
xkendrickz <kendrickchandra128@gmail.com>
2022-09-13 09:21:14 +0700

    guided data binding

---
+++ app/src/main/java/com/example/data_binding_10554/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.data_binding_10554
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel:ViewModel() {
+    val currentRandomFruitName: LiveData<String>
+        get() = FakeRepository.currentRandomFruitName
+
+    fun  onChangeRandomFruitClick() = FakeRepository.changeCurrentRandomFruitName()
+
+    val  editTextContent = MutableLiveData<String>()
+
+    private val _displayedEditTextContent = MutableLiveData<String>()
+    val displayedEditTextContent: LiveData<String>
+    get() = _displayedEditTextContent
+
+    fun onDisplayEditTextContentClick(){
+        _displayedEditTextContent.value = editTextContent.value
+    }
+
+    fun onSelectRandomEditTextFruit(){
+        editTextContent.value = FakeRepository.getRandomfruitName()
+    }
+}
